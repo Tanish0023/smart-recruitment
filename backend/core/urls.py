32b6715398b126df29842acb5a18d17b2a9515b5
Tanish0023 <tanish23aggarwal@gmail.com>
2026-02-14 21:01:38 +0530

    removed unused celery task

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
-from celery import shared_task
 
-@shared_task
-def trigger_error():
-    1 / 0
 
 urlpatterns = [
     path("admin/", admin.site.urls),
