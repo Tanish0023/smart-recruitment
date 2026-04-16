aba445dd249705320441934d5b3afad63a109ed9
Tanish0023 <tanish23aggarwal@gmail.com>
2026-04-16 18:00:08 +0530

    feat: Add @vercel/speed-insights package and integrate SpeedInsights component in main application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,12 @@ import App from "./App";
 import { ApolloProvider } from "@apollo/client/react";
 import client from "./apollo/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ApolloProvider client={client}>
     <App />
     <Analytics />
+    <SpeedInsights />
   </ApolloProvider>
 );
